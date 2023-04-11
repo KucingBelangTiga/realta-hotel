42db5751e275ffa5e25b7e2339488aaf19b040e7
ilhamsyah13 <ilham1304syah@gmail.com>
2023-04-11 11:54:43 +0700

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HotelsService } from './hotel/hotels/hotels.service';
 import { HotelsController } from './hotel/hotels/hotels.controller';
+import { HotelsController } from './hotel/hotels/hotels.controller';
+import { HotelsService } from './hotel/hotels/hotels.service';
 
 @Module({
   imports: [],
