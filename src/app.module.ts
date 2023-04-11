9cbaf1dd48b0f4b93868b8ebe270b010c03b2247
ilhamsyah13 <ilham1304syah@gmail.com>
2023-04-11 11:46:49 +0700

    coba

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HotelsService } from './hotel/hotels/hotels.service';
+import { HotelsController } from './hotel/hotels/hotels.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, HotelsController],
+  providers: [AppService, HotelsService],
 })
 export class AppModule {}
