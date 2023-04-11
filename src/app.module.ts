68cb1e38d5253f848bdc5f98e3f55be1dcfeaed2
ilhamsyah13 <ilham1304syah@gmail.com>
2023-04-11 11:58:23 +0700

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HotelsService } from './hotel/hotels/hotels.service';
+import { UsersController } from './hotel/users/users.controller';
 import { HotelsController } from './hotel/hotels/hotels.controller';
 import { HotelsService } from './hotel/hotels/hotels.service';
 import { HotelsController } from './hotel/hotels/hotels.controller';
@@ -10,7 +11,7 @@ import { HotelsService } from './hotel/hotels/hotels.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, HotelsController],
+  controllers: [AppController, HotelsController, UsersController],
   providers: [AppService, HotelsService],
 })
 export class AppModule {}
